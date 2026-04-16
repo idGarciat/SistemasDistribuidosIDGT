f72c87a57b8317096c1e9b7f389fd773c725c19e
idGarciat <ismaelgarci_88@hotmail.com>
2026-04-16 17:09:42 -0400

    Disertacion codigo

---
+++ RabbitMQ/src/main/java/com/mycompany/rabbitmq/RabbitMQ.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rabbitmq;
+
+/**
+ *
+ * @author PC1
+ */
+public class RabbitMQ {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
